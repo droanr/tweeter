40b31c4b0b89c8a91c7ce0c3b04bac9e42d02f5b
Droan Rishi <drishi@hearsaycorp.com>
2017-09-27 17:26:52 -0700

    add tbv cell

---
+++ tweeter/Views/TweetsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetsTableViewCell.swift
+//  tweeter
+//
+//  Created by Droan Rishi on 9/27/17.
+//  Copyright © 2017 Droan Rishi. All rights reserved.
+//
+
+import UIKit
+
+class TweetsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
